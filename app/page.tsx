45fb5248ce0b13708228e196c3ec9d533b877a29
ahuradel <ahuradel@gmail.com>
2024-11-23 00:21:22 +0330

    fix conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,29 @@ import ExhabitionProducts from "./components/ExhabitionProducts";
 import Products from "./components/Products";
 import QandA from "./components/QandA";
 
-const getBlogs = async ()=>{
+const getBlogs = async () => {
   try {
-    const req = await fetch(`${process.env.URL}/api`,{method:'GET' , headers:{'content-type':'application/json'}});
-    const {data} = await req.json();
-    return data
+    const req = await fetch(
+      `${process.env.URL ?? "http://localhost:3000"}/api`,
+      { method: "GET", headers: { "content-type": "application/json" } }
+    );
+    const { data } = await req.json();
+    return data;
   } catch (error) {
-    return {data :error}
+    return { data: error };
   }
-  }
-const Home =async ()=> {
-  const data =  await getBlogs();
+};
+const Home = async () => {
+  const data = await getBlogs();
 
   return (
     <main>
       <ExhabitionProducts />
       <Products />
-      <Blog data={data}/>
+      <Blog data={data} />
       <QandA />
     </main>
   );
-}
+};
 
-export default Home
+export default Home;
