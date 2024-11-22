1e32b1c547f4c7e0bf7907cce17f06f3fe84ea7e
ahuradel <ahuradel@gmail.com>
2024-11-22 22:17:51 +0330

    update fetching blog data config

--- app/api/blog/route.tsx
+++ app/api/blog/route.tsx
@@ -1,16 +1,8 @@
 import { NextResponse } from "next/server";
-import fs from "fs";
-import path from "path";
+import blogData from '../../../public/Data/blog.json'
 export async function GET() {
   try {
-    // Construct the full path to the JSON file
-    const filePath = path.join(process.cwd(), "public", "Data", "blog.json");
-
-    // Read the file's contents
-    const fileContents = fs.readFileSync(filePath, "utf8");
-
-    // const data = await response.json();;
-    const data = JSON.parse(fileContents);
+    const data = JSON.parse(JSON.stringify(blogData));
     return NextResponse.json({ data },{status:200});
   } catch (error) {
 
